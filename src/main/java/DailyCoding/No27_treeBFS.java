16afbdd30ea2144006cba6600388eb05ed4f7e66
philE22 <moonwhlking@gmail.com>
2022-08-24 10:37:12 +0900

    daily coding 27추가

--- src/main/java/DailyCoding/No27_treeBFS.java
+++ src/main/java/DailyCoding/No27_treeBFS.java
@@ -1,6 +1,7 @@
 package DailyCoding;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class No27_treeBFS {
     public static void main(String[] args) {
@@ -9,15 +10,34 @@ public class No27_treeBFS {
         tree rootChild2 = root.addChildNode(new tree("3"));
         tree leaf1 = rootChild1.addChildNode(new tree("4"));
         tree leaf2 = rootChild1.addChildNode(new tree("5"));
+        leaf1.addChildNode(new tree("6"));
+        rootChild2.addChildNode(new tree("7"));
         No27_treeBFS test = new No27_treeBFS();
+
+
         System.out.println(test.bfs(root));
     }
 
     public ArrayList<String> bfs(tree node) {
+        ArrayList<String> result = new ArrayList<>();
+        LinkedList<tree> queue = new LinkedList<>();
+        queue.add(node);
 
+        while (queue.size() != 0) {
+            //1. 현재 노드의 값을 result에 저장
+            tree tree = queue.poll();
+            result.add(tree.getValue());
+
+            //2. 현재 노드의 자식 노드들을 queue에 저장
+            if (tree.getChildrenNode() != null) {
+                for (int i = 0; i < tree.getChildrenNode().size(); i++) {
+                    queue.add(tree.getChildrenNode().get(i));
+                }
+            }
+        }
+
+        return result;
     }
-
-
 
 
     public static class tree {
@@ -30,7 +50,7 @@ public class No27_treeBFS {
         }
 
         public tree addChildNode(tree node) {
-            if(children == null) children = new ArrayList<>();
+            if (children == null) children = new ArrayList<>();
             children.add(node);
             return children.get(children.size() - 1);
         }
