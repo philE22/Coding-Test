ac348afc7cf516e6e7f00b73925ea11e933dc26e
philE22 <moonwhlking@gmail.com>
2022-09-02 10:43:03 +0900

    daily coding 31 해결

--- src/main/java/DailyCoding/No29_tiling.java
+++ src/main/java/DailyCoding/No29_tiling.java
@@ -12,25 +12,17 @@ public class No29_tiling {
         //       +2
         No29_tiling test = new No29_tiling();
 
-        int num = 43;
+        int num = 5;
         ArrayList<Integer> memo = new ArrayList<>(Arrays.asList(0, 1, 2));
-        System.out.println(test.aux(num, memo) + "!!!");
-        System.out.println(test.tiling(num));
+        System.out.println(test.aux(num, memo));
     }
 
-    public int tiling(int num) {
-        // TODO:
-        if (num == 1) return 1;
-        if (num == 2) return 2;
-
-
-        return tiling(num - 2) + tiling(num - 1);
-    }
 
 
     public int aux(int size, ArrayList<Integer> memo) {
         if(memo.size() > size) return memo.get(size);
 
+        System.out.println(memo);
         memo.add(aux(size - 1, memo) + aux(size - 2, memo));
         return memo.get(size);
     };
--- src/main/java/DailyCoding/No31_powerSet.java
+++ src/main/java/DailyCoding/No31_powerSet.java
@@ -1,16 +1,15 @@
 package DailyCoding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class No31_powerSet {
     public static void main(String[] args) {
-        String str = "jjump";
-
+        String str = "bbaaa";
         No31_powerSet test = new No31_powerSet();
         System.out.println(test.powerSet(str));
+
     }
 
     public ArrayList<String> powerSet(String str) {
@@ -18,7 +17,38 @@ public class No31_powerSet {
         // 각 요소들을 포함하는 경우의 수
         //
         char[] arr = str.toCharArray();
-        HashSet<String> set = new HashSet<>();
+        Arrays.sort(arr);
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+
+        for (char chr : arr) {
+            set.add(String.valueOf(chr));
+        }
+        Iterator<String> iterator = set.iterator();
+        ArrayList<String> list = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            list.add(iterator.next());
+        }
+
+        int length = list.size();
+        String output = "";
+        ArrayList<String> result = new ArrayList<>();
+        result.add("");
+
+        permutation(list, output, result, 0);
+        return result;
+    }
+
+    public String permutation(ArrayList<String> list, String output, ArrayList<String> result, int cnt) {
+        if (cnt == list.size()) return output;
+
+
+        String addoutput = output + list.get(cnt);
+        result.add(addoutput);
+        permutation(list, addoutput, result, cnt + 1);
+
+        permutation(list, output, result, cnt + 1);
+
         return null;
     }
 }
