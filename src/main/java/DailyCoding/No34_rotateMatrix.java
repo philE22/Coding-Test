8e381b38a9934dc2c9adb9e60b0a1156c82aaf31
philE22 <moonwhlking@gmail.com>
2022-09-14 10:31:38 +0900

    Daily Coding 34 구현, Baekjoon 11022 추가

--- src/main/java/DailyCoding/No34_rotateMatrix.java
+++ src/main/java/DailyCoding/No34_rotateMatrix.java
@@ -6,14 +6,12 @@ public class No34_rotateMatrix {
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
                 {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 16}
+                {5, 6, 7, 8}
         };
         int K = 2;
 
         No34_rotateMatrix test = new No34_rotateMatrix();
-        System.out.println(Arrays.deepToString(test.rotateMatrix(matrix, 1)));
+        System.out.println(Arrays.deepToString(test.rotateMatrix(matrix, 2)));
 
     }
 
@@ -24,24 +22,38 @@ public class No34_rotateMatrix {
         // int[1][i] 는 int[i][n-2]
         if(matrix == null) return null;
 
-        int N = matrix.length;
-        int[][] rotatedMatrix = new int[N][N];
+        int N = matrix.length;  //2
+        int M = matrix[0].length;  //4
+        int[][] rotatedMatrix = new int[M][N];
+        int[][] sameMatrix = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            sameMatrix[i] = Arrays.copyOf(matrix[i], M);
+        }
 
 
         while(K != 0){
 
             for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    rotatedMatrix[j][N - 1 - i] = matrix[i][j];
+                for(int j = 0; j < M; j++){
+                    rotatedMatrix[j][N - 1 - i] = sameMatrix[i][j];
                 }
             }
-
-            for (int i = 0; i < N; i++) {
-                matrix[i] = Arrays.copyOf(rotatedMatrix[i], N);
-            }
-
             K -= 1;
+
+            if (K == 0) return rotatedMatrix;
+
+            if (K != 0) {
+                for (int i = 0; i < M; i++) {
+                    for (int j = 0; j < N; j++) {
+                        sameMatrix[j][M - 1 - i] = rotatedMatrix[i][j];
+                    }
+                }
+                K -= 1;
+                if (K == 0) return sameMatrix;
+            }
         }
-        return rotatedMatrix;
+
+        return null;
     }
 }
